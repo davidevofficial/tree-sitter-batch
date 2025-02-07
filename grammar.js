3afb7d508e1ea58e00d1321ca6b7deed976c4d8f
davidevofficial <davidevufficial@gmail.com>
2025-02-07 22:00:36 +0100

    Added more grammar

--- grammar.js
+++ grammar.js
@@ -10,8 +10,55 @@
 module.exports = grammar({
   name: "batch",
 
+  extras: $ => [
+      /\s/  // Whitespace and comments
+    ],
+
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._statement),
+    _statement: $ => choice(
+        $.command,
+        $.label,
+        $.comment,
+        $.empty_line
+    ),
+
+    command: $ => seq(
+        optional('@'),
+        field('command', choice(
+          $.builtin_command,
+          $.identifier
+        )),
+        optional($.arguments)
+    ),
+
+        builtin_command: $ => choice(
+          'echo', 'set', 'if', 'else', 'for', 'goto',
+          'call', 'start', 'exit', 'pause', 'rem'
+        ),
+
+        arguments: $ => /.*/,
+
+        label: $ => seq(
+          ':',
+          field('name', $.identifier)
+        ),
+
+        variable: $ => seq(
+          '%',
+          field('name', $.identifier),
+          '%'
+        ),
+
+        comment: $ => token(seq(
+          choice('REM', '::'),
+          /.*/
+        )),
+
+        identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+        empty_line: $ => /\r?\n/
+
   }
 });
